e336c2c8d7ab192a0f96c16f91515d5d257d7d69
jeisson24 <rinconjeisson24@gmail.com>
2019-11-20 21:21:19 -0500

    add ingresar

--- resources/views/ingreso_apre/index.blade.php
+++ resources/views/ingreso_apre/index.blade.php
@@ -24,17 +24,18 @@
 				</div>
 			</div>
 
-				<div class="form-group">
+			<div class="form-group">
 				<label class="col-md-5 control-label"></label>
 				<div class="col-md-5"><br>
 				<button type="submit" class="btn btn-warning" >INGRESAR <span class="glyphicon glyphicon-send"></span></button>
-				</div></div>
-
+				</div>
+			</div>
+<hr>
      		<div class="container">
      		    <div class="row">
-					<div class="col-md-5">
+					<div class="col-md-3">
 							     		<div class="form-group">
-							    <label for="exampleFormControlSelect1">Equipo a Seleccionar</label>
+							    <label for="exampleFormControlSelect1">Seleccionar</label>
 							    <select class="form-control" id="exampleFormControlSelect1">
 							      <option>HP</option>
 							      <option>ASUS</option>
@@ -42,9 +43,50 @@
 							    </select>
 							  		</div>
 		     		</div>
-     	
+		     		 <table class="table table-hover table-sm">
+		     		 	<thead>
+                             <tr class="bg-secondary">
+                                 <th>ID</th>
+                                 <th>SERIAL</th>
+                                 <th>TIPO</th>
+                                 <th>COLOR</th>
+                                 <th>HORA INGRESO</th>
+                             </tr>
+                         </thead>
+                         <tbody>
+                             <th>121</th>
+                                 <th>ASA151</th>
+                                 <th>PORTATIL</th>
+                                 <th>VERDE</th>
+                                 <th>17:00</th>
+                         </tbody>
+                     </table>
+
+                     <button type="submit" class="btn btn-warning" >ACEPTAR <span class="glyphicon glyphicon-send"></span></button>
 				</div>
      </div>
+
+     <hr>
+
+<div class="form-group">
+	<h1>SALIDA DE EQUIPO</h1>
+	<br>
+				<label class="col-md-5 control-label" >INGRESA DOCUMENTO</label> 
+				<div class="col-md-5 inputGroupContainer">
+				<div class="input-group">
+				<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+				<input name="documento" placeholder="Documento" class="form-control"  type="text">
+				</div>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-5 control-label"></label>
+				<div class="col-md-5"><br>
+				<button type="submit" class="btn btn-warning" onclick="alert('byyyyyyy')">SALIR<span class="glyphicon glyphicon-send"></span></button>
+				</div>
+			</div>
+
 
 
 
